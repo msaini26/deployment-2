0c4245d5a563c1b580a2fb18ef906d371cad6b5e
Mansi S <mansisaini26@gmail.com>
2024-02-15 16:57:57 -0800

    finished ui touches

--- deployment-2/src/App.js
+++ deployment-2/src/App.js
@@ -2,42 +2,26 @@ import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
 import PrototypeResult from "./PrototypeResult";
 import ElevatorPitch from "./assets/Elevator_Pitch_Prototype.zip";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
+import Button from "@mui/material/Button";
 
- // Function will execute on click of button
- const onButtonClick = () => {
-     
+// function will download a zip file of elevator pitch prototype
+const downloadPrototype = () => {
   // using js fetch method to download elevator pitch zip file
   fetch(ElevatorPitch).then((response) => {
-      response.blob().then((blob) => {
-       
-          // Creating new object of PDF file
-          const fileURL =
-              window.URL.createObjectURL(blob);
-               
-          // Setting various property values
-          let alink = document.createElement("a");
-          alink.href = fileURL;
-          alink.download = "Elevator_Pitch.zip";
-          alink.click();
-      });
-  });
-};
+    response.blob().then((blob) => {
+      // Creating new object of PDF file
+      const fileURL = window.URL.createObjectURL(blob);
 
-const downloadExeFile = () => {
-  // file object
-  const file = new Blob([ElevatorPitch], { type: "application/octet-stream" });
-  // anchor link
-  const element = document.createElement("a");
-  element.href = URL.createObjectURL(file);
-  element.download = "Elevator_Pitch.exe";
-  // simulate link click
-  document.body.appendChild(element); // Required for this to work in FireFox
-  console.log(element);
-  element.click();
-  document.body.removeChild(element);
+      // Setting various property values
+      let alink = document.createElement("a");
+      alink.href = fileURL;
+      alink.download = "Elevator_Pitch.zip";
+      alink.click();
+    });
+  });
 };
 
 function Copyright() {
@@ -62,17 +46,29 @@ export default function App() {
           Download Interactive Prototype
         </Typography>
         <Typography variant="body1" align="left" sx={{ mb: 5 }}>
-          This is the second prototype deployment of Elevator Pitch.
+          Welcome to Elevator Pitch! Click the download button below to download
+          the interactive Unity prototype of Elevator Pitch.
         </Typography>
-        <div className="btnDiv">
-          <button id="downloadBtn" onClick={onButtonClick} value="download">
-            Download
-          </button>
-        </div>
+        <Typography variant="body2" align="left" sx={{ mb: 5 }}>
+          1. Click the download button below to download the interactive Unity
+          prototype of Elevator Pitch. <br></br> 2. Unzip the downloaded file.
+          <br></br>
+          3. Open the unzipped folder and double click on the Elevator_Pitch.exe
+          file to run the prototype. <br></br>
+          4. Use your mouse or trackpad to experiment in the prototype
+          environment. Enjoy! 🎉
+        </Typography>
+        <Button
+          variant="outlined"
+          sx={{ mb: 5 }}
+          onClick={downloadPrototype}
+          value="download"
+        >
+          Download Prototype
+        </Button>
 
         <PrototypeResult />
 
-     
         <Copyright />
       </Box>
     </Container>
