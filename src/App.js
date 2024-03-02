9bccc031d21cda45cb24dd226075b1b2f09c8ad5
Mansi S <mansisaini26@gmail.com>
2024-03-01 17:10:53 -0800

    updated based on fonts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import PrototypeResult from "./PrototypeResult";
 import ElevatorPitch from "./assets/Elevator_Pitch_Prototype.zip";
 import Button from "@mui/material/Button";
 
+// font importing
+import "./font.css";
+import "./fonts/Verve.ttf";
+import "./fonts/VerveAlternate.ttf";
+import "./fonts/VerveShadow.ttf";
+import "./fonts/Titania-Outline.ttf";
+import "./fonts/Titania-Shadow.ttf";
+import "./fonts/Titania-Regular.ttf";
+import "./fonts/Book.otf";
+import "./fonts/Chopper.ttf"
+
 // function will download a zip file of elevator pitch prototype
 const downloadPrototype = () => {
   // using js fetch method to download elevator pitch zip file
@@ -38,24 +49,26 @@ export default function App() {
   return (
     <Container maxWidth="md">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h2" component="h1" sx={{ mb: 5 }}>
+        <Typography variant="h2" component="h1" sx={{ mb: 5, fontFamily: "VerveRegular" }}>
           Elevator Pitch Deployment #2
-        </Typography>
+        </Typography>  
 
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2, fontFamily: "VerveAlt" }}>
           Download Interactive Prototype
         </Typography>
-        <Typography variant="body1" align="left" sx={{ mb: 5 }}>
+        <Typography variant="body1" align="left" sx={{ mb: 5, fontFamily: "Chop", letterSpacing: "3px" }}>
           Welcome to Elevator Pitch! Click the download button below to download
           the interactive Unity prototype of Elevator Pitch.
         </Typography>
-        <Typography variant="body2" align="left" sx={{ mb: 5 }}>
+        <Typography variant="body2" align="left" sx={{ mb: 5, fontFamily: "Chop", letterSpacing: "3px", }}>
           1. Click the download button below to download the interactive Unity
           prototype of Elevator Pitch.
           <br></br>
           <strong><i>Note: It may take a second to download depending on your internet speed.</i></strong>
         <br></br>
+        <br></br>
           2. Unzip the downloaded folder.
+          <br></br>
           <br></br>
           3. Open the unzipped folder and double click on the{" "}
           <strong>
@@ -63,12 +76,13 @@ export default function App() {
           </strong>
           file to run the prototype.
           <br></br>
+          <br></br>
           4. Use your mouse or trackpad to experiment in the prototype
           environment. Enjoy! 🎉
         </Typography>
         <Button
           variant="outlined"
-          sx={{ mb: 5 }}
+          sx={{ mb: 5, fontSize: "20px", fontFamily: "TitaniaRegular", textTransform: "none" }}
           onClick={downloadPrototype}
           value="download"
         >
--- src/PrototypeResult.js
+++ src/PrototypeResult.js
@@ -20,10 +20,10 @@ export default function PrototypeResult() {
           alt="prototype result"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h5" component="div" sx={{fontFamily: "VerveRegular"}}>
             Lo-fi Push-and-Pull Mechanic Preview #1
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography variant="body2" color="text.secondary" sx={{fontFamily: "Chop", letterSpacing: "1px",}}>
             Here is an image of what the player might encounter in the
             interactive prototype.
           </Typography>
@@ -38,10 +38,10 @@ export default function PrototypeResult() {
           alt="another prototype"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h5" component="div" sx={{fontFamily: "VerveRegular"}}>
             Lo-fi Push-and-Pull Mechanic Preview #2
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography variant="body2" color="text.secondary" sx={{fontFamily: "Chop", letterSpacing: "1px"}}>
             Here is another preview image for the interactive prototype of what
             the player might encounter.
           </Typography>
